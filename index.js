69ea8f72f66e9afdde72dbf8814907af0a327b77
skyGwork <skyGworkk@gmail.com>
2022-09-14 21:00:36 +0530

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+const gameStartBtn = document.getElementById('gameStartBtn');
+
+function startGame() {
+  console.log('Game is starting .....');
+}
+startGame();
+
+gameStartBtn.addEventListener('click',() => {
+   console.log('the game is on..'); 
+})
